25be8e36fb853148e3c42e370ba2b23685da2139
prashant13031998 <prashanttomar198@gmail.com>
2023-05-08 13:01:57 +0530

    chnages in backend url

--- client/src/components/SingleChat.js
+++ client/src/components/SingleChat.js
@@ -15,7 +15,8 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "http://localhost:5000"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
+const ENDPOINT = "https://mern-chat-app-service.onrender.com";
+// const ENDPOINT = "http://localhost:5000"; // "https://mern-chat-app-service.onrender.com"; -> After deployment
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
